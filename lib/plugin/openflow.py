5158f5342ca9b0bcc8ede0b9b40d38c6ca192d7d
Felician Nemeth <nemethf@tmit.bme.hu>
2017-03-30 17:48:19 +0200

    plugin/openflow: Simplify prepare_rules(), comments

--- lib/plugin/openflow.py
+++ lib/plugin/openflow.py
@@ -16,6 +16,7 @@ def configure_remote_vnf(nfpa, vnf_function, traffictype):
     log = l.getLogger(__name__, config['LOG_LEVEL'], config['app_start_date'],
                       config['LOG_PATH'])
     of_path = config["MAIN_ROOT"] + "/of_rules/"   # path to the openflow rules
+    bidir = int(config["biDir"]) == 1
     def invoke1(cmd, msg):
         log.debug("%s with %s" % (msg, cmd))
         invoke(command=cmd, logger=log, email_adapter=config['email_adapter'])
@@ -32,12 +33,10 @@ def configure_remote_vnf(nfpa, vnf_function, traffictype):
         log.error("More info: http://ios.tmit.bme.hu/nfpa")
         open(filename) # Raise exception
 
-    prepare_rules = lambda path, bidir: \
+    prepare_rules = lambda path: \
         flow_prep.prepareOpenFlowRules(log, of_path, path,
                                        config["control_vnf_inport"],
                                        config["control_vnf_outport"], bidir)
-    
-    bidir = int(config["biDir"]) == 1
 
     # First, delete the flows
     ofctl_cmd = config["control_path"] + " " + \
@@ -61,43 +60,37 @@ def configure_remote_vnf(nfpa, vnf_function, traffictype):
             # Change flow rule file
             scenario_path = scenario_path.replace("unidir", "bidir")
 
-        scenario_path = prepare_rules(scenario_path, bidir)
+        scenario_path = prepare_rules(scenario_path)
         cmd = ofctl_cmd.replace("<C>", "add-flows") + scenario_path
         invoke1(cmd, "Adding flows")
         return True
 
     ############     OTHER CASES    ###########
-    # Check whether flow rules exists
-    # Convention vnf_function.trace_direction.flows
+    # Filename convention: vnf_function.trace_direction.flows
+    if bidir:
+        log.error("Bi-directional scenario for this VNF is not yet supported")
+        log.error("Check the value of 'biDir' in nfpa.cfg")
+        return False
+
     scenario_path = vnf_function + "." + traffictype + "_unidir.flows"
     check_file_exists(scenario_path, traffictype)
 
-    # If flow file exists try to find corresponding groups
+    # Try to find file for group rules
     scenario_path = scenario_path.replace(".flows", ".groups")
     log.info("Looking for group file: %s" % scenario_path)
     if (os.path.isfile(str(of_path + scenario_path))):
-        log.info("Group file found for this scenario: %s" % scenario_path)
+        log.debug("Group file found: %s" % scenario_path)
         # Prepare group file, i.e., replace port related metadata
-        group_path = prepare_rules(scenario_path, False) #TODO: bidir handling
+        group_path = prepare_rules(scenario_path)  # TODO: bidir handling
         cmd = ofctl_cmd.replace("<C>", "add-groups")
         cmd += " " + group_path
         invoke1(cmd, "Adding groups")
     else:
         log.info("No group file was found...continue")
 
-    # Change back to the .flows file from .groups
     scenario_path = scenario_path.replace(".groups", ".flows")
-
-    #if biDir is set, another file has the rules for reverse direction
-    if bidir:
-        log.error("Configuring your VNF by NFPA for bi-directional scenario " +
-                  "is currently not supported")
-        log.error("Check the value of 'biDir' in nfpa.cfg")
-        return False
-
-    # Replace metadata in flow rule files
-    scenario_path = prepare_rules(scenario_path, bidir)
-    # Assemble command ovs-ofctl
+    # Replace metadata (e.g., port numbers) in flow rules
+    scenario_path = prepare_rules(scenario_path)
     cmd = ofctl_cmd.replace("<C>", "add-flows") + scenario_path
     invoke1(cmd, "Adding flows")
     return True
